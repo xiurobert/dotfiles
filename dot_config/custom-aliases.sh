62a6316d08c7327761da0f875bae4e486d6676b0
Robert <xiurobert@gmail.com>
2022-06-03 01:48:35 +0800

    changed some stuff (commited via bash alias)

--- dot_config/custom-aliases.sh
+++ dot_config/custom-aliases.sh
@@ -3,11 +3,13 @@ alias pbcopy="xclip -sel clip"
 
 function _df_upd_helper() {
     git add -A
-    git commit -am "changed some stuff (commited via bash alias)"
+    git commit -am "changed some stuff (committed via bash alias)"
     git push
 }
 
 function dotfile_update() {
     chezmoi re-add
+    curr_dir = $(pwd)
     cd $(chezmoi source-path) && _df_upd_helper
+    cd curr_dir
 }
