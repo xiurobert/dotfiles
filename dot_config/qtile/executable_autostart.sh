1bda4355f237a80d8eabeafe80376a99d8ca4b17
Robert <xiurobert@gmail.com>
2022-06-07 20:15:28 +0800

    feat: switch to dp-4

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xwallpaper --center "$HOME"/wallpapers/mojave-day.jpg &
 picom -b & 
-xrandr --output HDMI-0 --mode 3440x1440 --rate 100 &
+xrandr --output DP-4 --mode 3440x1440 --rate 100 &
 exec volctl &
