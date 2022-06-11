8afd096561a91d30a9f64efe02c39b2c2074387b
Robert <xiurobert@gmail.com>
2022-06-11 17:47:35 +0800

    updated dotfiles without description (commit via dotfile_update)

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xwallpaper --center "$HOME"/wallpapers/mojave-day.jpg &
 picom -b & 
-xrandr --output DP-4 --mode 3440x1440 --rate 100 &
+xrandr --output DP-2 --mode 3440x1440 --rate 100 &
 exec nm-applet &
 exec volctl &
