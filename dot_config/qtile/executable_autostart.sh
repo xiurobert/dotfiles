63a913f158f1aff5c7b6fd71f4e84248b0b45f1d
Robert <xiurobert@gmail.com>
2022-06-20 09:52:27 +0800

    feat: fix tearing on nvididia + autostart solaar

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -2,5 +2,7 @@
 xwallpaper --center "$HOME"/wallpapers/mojave-day.jpg &
 picom -b & 
 xrandr --output DP-2 --mode 3440x1440 --rate 100 &
+nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }" &
 exec nm-applet &
 exec volctl &
+exec solaar &
