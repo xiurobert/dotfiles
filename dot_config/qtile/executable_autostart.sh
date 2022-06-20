aa95ac9492e08b12d5330eef6487e23c83986047
Robert <xiurobert@gmail.com>
2022-06-20 09:53:22 +0800

    feat: move nvidia fix bx before xrandr to avoid breaking refresh rate

--- dot_config/qtile/executable_autostart.sh
+++ dot_config/qtile/executable_autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xwallpaper --center "$HOME"/wallpapers/mojave-day.jpg &
-picom -b & 
-xrandr --output DP-2 --mode 3440x1440 --rate 100 &
+picom -b &
 nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }" &
+xrandr --output DP-2 --mode 3440x1440 --rate 100 &
 exec nm-applet &
 exec volctl &
 exec solaar &
