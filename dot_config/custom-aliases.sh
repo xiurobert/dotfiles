7fb731fe4babeeccc60efcf067ccd8080fcb8dae
Robert <xiurobert@gmail.com>
2022-07-16 19:55:28 +0800

    updated dotfiles without description (commit via dotfile_update)

--- dot_config/custom-aliases.sh
+++ dot_config/custom-aliases.sh
@@ -4,10 +4,13 @@ alias pbcopy="xclip -sel clip"
 alias py="python"
 complete -F _python py
 alias ipy="ipython"
+alias refreshenv="source ~/.bashrc"
 
 alias tail_qtile_log="tail -f $HOME/.local/share/qtile/qtile.log"
 
 alias install-pytorch="pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116"
+alias clean-pytorch="pip3 uninstall torch torchvision torchaudio --yes"
+alias clean-inst-pytorch="clean-pytorch && install-pytorch"
 
 function _df_upd_helper() {
     git add -A
